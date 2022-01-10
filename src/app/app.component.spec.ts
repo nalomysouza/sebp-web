f00e0f9ee17be44e4db2e5d35192c4ce0e448278
Nalomy de Souza Alves <nalomysouza@gmail.com>
2022-01-09 21:10:38 -0300

    Atualizando libs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sebponline-web'`, () => {
+  it(`should have as title 'sebp-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sebponline-web');
+    expect(app.title).toEqual('sebp-web');
   });
 
 });
