a4aec7a1ea7c06bdaf61b8e6cb592f5f5a4056d0
NALOMY DE SOUZA ALVES OLIVEIRA <nalomysouza@gmail.com>
2022-02-12 00:08:32 -0300

    cobertura de testes biblioteca-form

--- src/app/pages/biblioteca/biblioteca-form/biblioteca-form.component.spec.ts
+++ src/app/pages/biblioteca/biblioteca-form/biblioteca-form.component.spec.ts
@@ -1,25 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { BibliotecaFormComponent } from './biblioteca-form.component';
 
 describe('BibliotecaFormComponent', () => {
   let component: BibliotecaFormComponent;
-  let fixture: ComponentFixture<BibliotecaFormComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [BibliotecaFormComponent]
-    })
-      .compileComponents();
-  });
+  let routerStub: any;
+  let activatedRouteStub: any;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BibliotecaFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    routerStub = {
+      url: '/test',
+      navigate: jest.fn()
+    }
+    activatedRouteStub = {
+      queryParams: of({ codigoBiblioteca: 1 })
+    }
+    component = new BibliotecaFormComponent(routerStub, activatedRouteStub);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should ngOnInit', () => {
+    component.ngOnInit();
+    expect(component.codigoBiblioteca).toBe(1);
+    expect(component.desabilitarRotas).toBeFalsy();
+  });
+
+  describe('isActiveRouter', () => {
+    it('resultado true', () => {
+      expect(component.isActiveRouter('/test')).toBe(true);
+    });
+    it('resultado false', () => {
+      expect(component.isActiveRouter('/')).toBe(false);
+    });
+  });
+
+  describe('proximaRota', () => {
+    it('resultado true', () => {
+      component.proximaRota('/test');
+      expect(routerStub.navigate).toBeCalled();
+    });
   });
 });
